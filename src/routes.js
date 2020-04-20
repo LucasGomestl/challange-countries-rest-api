b4830f5ffb4530478194f4adb9709d65b2451d0d
Lucas Gomes <lucasgomes_tl@hotmail.com>
2020-04-20 18:57:58 -0400

    Define routes

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import Detail from "./components/Detail";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/detail/:countryName" component={Detail} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
